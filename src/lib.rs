d0bd0873925d678e681425787a0c22ae34aa6b34
madmaxio <sprayrules@gmail.com>
2016-03-28 20:32:03 +0300

    fixing travis

--- src/lib.rs
+++ src/lib.rs
@@ -128,7 +128,7 @@ pub fn connect(addr: String) -> Result<Connection> {
 
 #[test]
 fn connect_and_query() {
-    let mut conn = connect("127.0.0.1:9042".to_string()).unwrap();
+    let mut conn = connect("localhost:9042".to_string()).unwrap();
 
     let result = conn.query("DROP KEYSPACE IF EXISTS testing".to_string(), Consistency::Quorum);
     println!("Result of DROP KEYSPACE was {:?}", result);
